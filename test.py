4a7b962e8b6279199d87d00beeefffe5b367f58a
mohanbabud <dusanapudimohan@gmail.com>
2022-02-13 23:44:06 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ test.py
@@ -0,0 +1,80 @@
+def vol(event, context):
+    pass
+
+
+import logging
+import boto3
+import json
+import os
+from pprint import pprint
+
+ec2 = boto3.client("ec2")
+response = ec2.describe_volumes()
+volumesbyid = {}
+volumebyinsvol = {}
+volumestatuses = {'ok': 0, 'impaired': 1, 'warning': 2, 'passed': 3, 'insufficient-data': 4}
+volumestates = {'available': 0, 'creating': 1, 'deleted': 2, 'deleting': 3, 'in-use': 4}
+volumes = {}
+
+
+for volume in response['Volumes']:
+    VolumeId = volume['VolumeId']
+    Attach = volume['Attachments']
+    info = volumes.setdefault(VolumeId, {})
+    info['volume_state'] = volume['State']
+    #    info['InstanceID'] = volume['InstanceId']
+    for volid, volinfo in volumes.items():
+        VolumeStatus = volinfo['volume_state']
+        statecode = volumestates.get(VolumeStatus, -1)
+        info2 = volumesbyid.setdefault(volid, {})
+        info2['volume_state'] = statecode
+        for k in Attach:
+           InstanceId = k['InstanceId']
+           info3 = volumesbyid.setdefault(volid, {})
+           info3['InstanceId'] = InstanceId
+
+
+response = ec2.describe_volume_status()
+
+for volume in response['VolumeStatuses']:
+    VolumeId = volume['VolumeId']
+    VolumeStatus = volume['VolumeStatus']
+    details = VolumeStatus['Details']
+    Status2 = VolumeStatus['Status']
+    info = volumesbyid.setdefault(VolumeId,{})
+    statecode = volumestatuses.get(Status2, -1)
+    info['vol_status'] = statecode
+
+
+
+
+#*** Below Block is to get io-enabled status***##
+
+
+#    for detail in details:
+#        if detail['Name'] == 'io-enabled':
+#            VolumeStatus = detail['Status']
+#            statecode = volumestatuses.get(VolumeStatus, -1)
+#            info = volumesbyid.setdefault(VolumeId, {})
+#            info['volume_status'] = statecode
+
+for x,v in volumesbyid.items():
+    if 'InstanceId' in v:
+        def put_ec2_volume_metrics():
+            namespace = 'EBS'
+            dimensions = [volumesbyid(Name='VolumeID',Value=VolumeId)]
+            metrics = [volumesbyid(MetricName='volume_state',value=volume_state,Dimesnsions=dimensions),]
+            cloudwatch.put_metric_data(Namespace=namespace,MetricData=metrics)
+            print('yes')
+    else:
+
+        def put_ec2_volume_metrics(volumeid,volum_state, volume_status):
+            namespace = 'EBS'
+            dimensions = [volumesbyid(Name='VolumeID', Value=VolumeId)]
+            metrics = [volumesbyid(MetricName='volume_state', value=volume_state, Dimesnsions=dimensions),]
+            print('No')
+            cloudwatch.put_metric_data(Namespace=namespace, MetricData=metrics)
+
+
+print('=' * 30)
+pprint(volumesbyid)
