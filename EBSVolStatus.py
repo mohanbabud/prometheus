9331e675998e0e3f3dd8a83987563d8c604c0818
mohanbabud <dusanapudimohan@gmail.com>
2022-02-13 15:39:08 +0530

    Added Instance ID and Attachment Status

--- EBSVolStatus.py
+++ EBSVolStatus.py
@@ -12,11 +12,11 @@ ec2 = boto3.client("ec2")
 response = ec2.describe_volumes()
 volumesbyid = {}
 volumebyinsvol = {}
-volumestatuses = {'ok': 0, 'impaired': 1, 'warning': 2,'insufficient-data': 3}
+volumestatuses = {'ok': 0, 'impaired': 1, 'warning': 2, 'passed': 3, 'insufficient-data': 4}
 volumestates = {'available': 0, 'creating': 1, 'deleted': 2, 'deleting': 3, 'in-use': 4}
 volumes = {}
 
-print(response)
+
 for volume in response['Volumes']:
     VolumeId = volume['VolumeId']
     Attach = volume['Attachments']
@@ -29,12 +29,9 @@ for volume in response['Volumes']:
         info2 = volumesbyid.setdefault(volid, {})
         info2['volume_state'] = statecode
         for k in Attach:
-            if k['State'] == 'attached':
-               InstanceId = k['InstanceId']
-               info3 = volumesbyid.setdefault(volid, {})
-               info3['InstanceId'] = InstanceId
-            else:
-                pass
+           InstanceId = k['InstanceId']
+           info3 = volumesbyid.setdefault(volid, {})
+           info3['InstanceId'] = InstanceId
 
 
 response = ec2.describe_volume_status()
@@ -43,21 +40,27 @@ for volume in response['VolumeStatuses']:
     VolumeId = volume['VolumeId']
     VolumeStatus = volume['VolumeStatus']
     details = VolumeStatus['Details']
-    Status2 = VolumeStatus['Status']
-    info = volumesbyid.setdefault(VolumeId,{})
-    statecode = volumestatuses.get(Status2, -1)
-    info['vol_status'] = statecode
-
-
-# Below Block is to fetch the IO Enabled Status
-
-#    for detail in details:
-#        if detail['Name'] == 'io-enabled':
-#            VolumeStatus = detail['Status']
-#            statecode = volumestatuses.get(VolumeStatus, -1)
-#            info = volumesbyid.setdefault(VolumeId, {})
-#            info['volume_status'] = statecode
-
+    for detail in details:
+        if detail['Name'] == 'io-enabled':
+            VolumeStatus = detail['Status']
+            statecode = volumestatuses.get(VolumeStatus, -1)
+            info = volumesbyid.setdefault(VolumeId, {})
+            info['volume_status'] = statecode
+for x,v in volumesbyid.items():
+    if 'InstanceId' in v:
+       def put_ec2_volume_metrics(instanceid,volumeid,volume_state,volume_status):
+           namespace='EBS'
+           dimensions = [volumesbyid(Name='VolumeID',Value=VolumeId)]
+           metrics = [ volumesbyid(MetricName='volume_state',value=volume_state,Dimesnsions=dimensions)]
+           cloudwatch.put_metric_data(Namespace=namespace,MetricData=metrics)
+           print(metrics)
+    else:
+        def put_ec2_volume_metrics(volumeid, volume_state, volume_status):
+            namespace = 'EBS'
+            dimensions = [volumesbyid(Name='VolumeID', Value=VolumeId)]
+            metrics = [volumesbyid(MetricName='volume_state', value=volume_state, Dimesnsions=dimensions)]
+            print(metrics)
+            cloudwatch.put_metric_data(Namespace=namespace, MetricData=metrics)
 
 
 print('=' * 30)
