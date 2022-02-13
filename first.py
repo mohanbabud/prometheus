32cc437010a281387f44a35b70f0c7f50691e618
mohanbabud <dusanapudimohan@gmail.com>
2022-02-13 10:11:31 +0530

    Volume State and Status with volumeID as Key working

--- first.py
+++ first.py
@@ -1,5 +1,4 @@
 import boto3
-import json
 
 #ec2 = boto3.client('ec2',aws_access_key_id=AKIA4BKIPYPIOVAYV6MZ,aws_secret_access_key=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,region_name='us-east-1')
 #ec2 = boto3.client("ec2")
